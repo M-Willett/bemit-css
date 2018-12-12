45921b3dbc6dc61ae24ea4e385cb1ada8a59f37b
Matt Willett <mattwillettuk@gmail.com>
2018-12-12 16:33:05 +0000

    improvement: sidebar styling (markup)

--- src/ui/AppSidebar.jsx
+++ src/ui/AppSidebar.jsx
@@ -8,23 +8,23 @@ export default class AppSidebar extends React.Component {
             <aside className={'c-sidebar c-sidebar--fixed u-scrollable u-br'}>
                 <div className={'c-sidebar__inner u-p'}>
                     {/* Settings */}
-                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-pt--md'} href={'#section_settings'}>Settings</a>
+                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-mt--md u-pl--rg'} href={'#section_settings'}>Settings</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#settings_core'}>Core settings</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#settings_theme'}>Theme settings</a>
 
                     {/* Resets */}
-                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-pt--md'} href={'#section_resets'}>Resets</a>
+                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-mt--xl u-pl--rg'} href={'#section_resets'}>Resets</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#reset_user_agent'}>User Agent</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#reset_box_sizing'}>Box Sizing</a>
 
                     {/* Elements */}
-                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-pt--md'} href={'#section_elements'}>Elements</a>
+                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-mt--xl u-pl--rg'} href={'#section_elements'}>Elements</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#element_body'}>Body</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#element_scrollbar'}>Scrollbar</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#element_typography'}>Typography</a>
 
                     {/* Objects */}
-                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-pt--md'} href={'#section_objects'}>Objects</a>
+                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-mt--xl u-pl--rg'} href={'#section_objects'}>Objects</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#object_block'}>Block</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#object_flex'}>Flex</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#object_grid'}>Grid</a>
@@ -32,7 +32,7 @@ export default class AppSidebar extends React.Component {
                     {/* <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#object_wrapper'}>Wrapper</a> */}
 
                     {/* Components */}
-                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-pt--md'} href={'#section_components'}>Components</a>
+                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-mt--xl u-pl--rg'} href={'#section_components'}>Components</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#component_button'}>Button</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#component_code'}>Code</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#component_header'}>Header</a>
@@ -41,7 +41,7 @@ export default class AppSidebar extends React.Component {
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#component_switch'}>Switch</a>
 
                     {/* Utilities */}
-                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-pt--md'} href={'#section_utilities'}>Utilities</a>
+                    <a className={'c-sidebar__list__link c-sidebar__list__link--parent o-block u-mb--xs u-font--bold u-mt--xl u-pl--rg'} href={'#section_utilities'}>Utilities</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#utility_background'}>Background</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#utility_border'}>Border</a>
                     <a className={'c-sidebar__list__link o-block u-mb--xs u-pl--rg'} href={'#utility_margin'}>Margin</a>
