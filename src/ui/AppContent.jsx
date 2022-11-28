796083bfa8638e8120e4ffac8cc1fb7796cb7072
Matt Willett <mattwillettuk@gmail.com>
2022-11-28 11:39:51 +0000

    limit width of content with new max width util

--- src/ui/AppContent.jsx
+++ src/ui/AppContent.jsx
@@ -1,85 +1,95 @@
-import React from 'react';
-import AppFooter from './AppFooter';
+import React from "react";
+import AppFooter from "./AppFooter";
 
-import SettingCore from './Components/SettingCore';
-import SettingTheme from './Components/SettingTheme';
+import SettingCore from "./Components/SettingCore";
+import SettingTheme from "./Components/SettingTheme";
 
-import ResetUserAgent from './Components/ResetUserAgent';
-import ResetBoxSizing from './Components/ResetBoxSizing';
+import ResetUserAgent from "./Components/ResetUserAgent";
+import ResetBoxSizing from "./Components/ResetBoxSizing";
 
-import ElementBody from './Components/ElementBody';
-import ElementScrollbar from './Components/ElementScrollbar';
-import ElementTypography from './Components/ElementTypography';
+import ElementBody from "./Components/ElementBody";
+import ElementScrollbar from "./Components/ElementScrollbar";
+import ElementTypography from "./Components/ElementTypography";
 
-import ObjectBlock from './Components/ObjectBlock';
-import ObjectFlex from './Components/ObjectFlex';
-import ObjectGrid from './Components/ObjectGrid';
-import ObjectTable from './Components/ObjectTable';
+import ObjectBlock from "./Components/ObjectBlock";
+import ObjectFlex from "./Components/ObjectFlex";
+import ObjectGrid from "./Components/ObjectGrid";
+import ObjectTable from "./Components/ObjectTable";
 
-import ComponentHeader from './Components/ComponentHeader';
-import ComponentFooter from './Components/ComponentFooter';
-import ComponentButton from './Components/ComponentButton';
-import ComponentCode from './Components/ComponentCode';
-import ComponentSwitch from './Components/ComponentSwitch';
+import ComponentHeader from "./Components/ComponentHeader";
+import ComponentFooter from "./Components/ComponentFooter";
+import ComponentButton from "./Components/ComponentButton";
+import ComponentCode from "./Components/ComponentCode";
+import ComponentSwitch from "./Components/ComponentSwitch";
 
-import UtilityBackground from './Components/UtilityBackground';
-import UtilityBorder from './Components/UtilityBorder';
-import UtilityMargin from './Components/UtilityMargin';
-import UtilityPadding from './Components/UtilityPadding';
-
+import UtilityBackground from "./Components/UtilityBackground";
+import UtilityBorder from "./Components/UtilityBorder";
+import UtilityMargin from "./Components/UtilityMargin";
+import UtilityPadding from "./Components/UtilityPadding";
 
 export default class AppContent extends React.Component {
+  render() {
+    return (
+      <main className={"c-content u-scrollable o-flex--1"}>
+        <div className={"u-p--xxl u-mx--auto u-width--max--1200"}>
+          <p className={"u-h1 u-mt--xxl"} id={"section_settings"}>
+            Settings
+          </p>
+          <SettingCore />
+          <SettingTheme />
 
-    render(){
-        return (
-            <main className={'c-content u-scrollable o-flex--1'}>
-                <div className={'u-p--xxl u-ml--xl u-mr--xl'}>
-                    <p className={'u-h1 u-mt--xxl'} id={'section_settings'}>Settings</p>
-                    <SettingCore/>
-                    <SettingTheme/>
+          <p className={"u-h1 u-pt--xxl u-mt--xxl"} id={"section_resets"}>
+            Resets
+          </p>
+          <ResetUserAgent />
+          <ResetBoxSizing />
 
-                    <p className={'u-h1 u-pt--xxl u-mt--xxl'} id={'section_resets'}>Resets</p>
-                    <ResetUserAgent/>
-                    <ResetBoxSizing/>
+          <p className={"u-h1 u-pt--xxl u-mt--xxl"} id={"section_elements"}>
+            Elements
+          </p>
+          <ElementBody />
+          <ElementTypography />
+          <ElementScrollbar />
 
-                    <p className={'u-h1 u-pt--xxl u-mt--xxl'} id={'section_elements'}>Elements</p>
-                    <ElementBody/>
-                    <ElementTypography/>
-                    <ElementScrollbar/>
+          <p className={"u-h1 u-pt--xxl u-mt--xxl"} id={"section_objects"}>
+            Objects
+          </p>
+          <ObjectBlock />
+          <ObjectFlex />
+          <ObjectGrid />
+          <ObjectTable />
+          {/* <ObjectWrapper/> */}
 
-                    <p className={'u-h1 u-pt--xxl u-mt--xxl'} id={'section_objects'}>Objects</p>
-                    <ObjectBlock/>
-                    <ObjectFlex/>
-                    <ObjectGrid/>
-                    <ObjectTable/>
-                    {/* <ObjectWrapper/> */}
+          <p className={"u-h1 u-pt--xxl u-mt--xxl"} id={"section_components"}>
+            Components
+          </p>
+          <ComponentButton />
+          <ComponentCode />
+          <ComponentHeader />
+          <ComponentFooter />
+          {/* <ComponentSidebar/> */}
+          <ComponentSwitch />
 
-                    <p className={'u-h1 u-pt--xxl u-mt--xxl'} id={'section_components'}>Components</p>
-                    <ComponentButton/>
-                    <ComponentCode/>
-                    <ComponentHeader/>
-                    <ComponentFooter/>
-                    {/* <ComponentSidebar/> */}
-                    <ComponentSwitch/>
+          <p className={"u-h1 u-pt--xxl u-mt--xxl"} id={"section_utilities"}>
+            Utilities
+          </p>
+          <UtilityBackground />
+          <UtilityBorder />
+          {/* <UtilityClearfix/> */}
+          {/* <UtilityColour/> */}
+          {/* <UtilityHeading/> */}
+          <UtilityMargin />
+          <UtilityPadding />
+          {/* <UtilityPosition/> */}
+          {/* <UtilityScroll/> */}
+          {/* <UtilityTypography/> */}
+          {/* <UtilityWidth/> */}
+          {/* <Utility/> */}
+          {/* <Utility/> */}
 
-                    <p className={'u-h1 u-pt--xxl u-mt--xxl'} id={'section_utilities'}>Utilities</p>
-                    <UtilityBackground/>
-                    <UtilityBorder/>
-                    {/* <UtilityClearfix/> */}
-                    {/* <UtilityColour/> */}
-                    {/* <UtilityHeading/> */}
-                    <UtilityMargin/>
-                    <UtilityPadding/>
-                    {/* <UtilityPosition/> */}
-                    {/* <UtilityScroll/> */}
-                    {/* <UtilityTypography/> */}
-                    {/* <UtilityWidth/> */}
-                    {/* <Utility/> */}
-                    {/* <Utility/> */}
-
-                    <AppFooter/>
-                </div>
-            </main>
-        )
-    }
+          <AppFooter />
+        </div>
+      </main>
+    );
+  }
 }
